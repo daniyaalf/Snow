0ce4328ae5b6ee13f849a78506cc86744c433086
daniyaalf14 <dquantum14@gmail.com>
2024-03-16 16:27:25 -0400

    Timer works as intended now
    The timer works as intended now; I accomplished this using a global scene that tracks the number of bateyes killed which is kind of bad coding wise and if i can find a better way I'll do that because the player stats are also currently global. Not good practice for future. But the timer does stop when you die or when you kill every enemy so for now this is fine. Next I need a counter on how many bateyes are dead which is pretty easy at this point.

--- UI/time_panel.gd
+++ UI/time_panel.gd
@@ -4,12 +4,18 @@ var time: float = 0.0
 var minutes: int = 0
 var seconds: int = 0
 var msec: int = 0
+var stats = PlayerStats
+var enemyGlobalStats = EnemyGlobalStats
+
+func _ready():
+	stats.connect("no_health", stop)
+	enemyGlobalStats.connect("no_enemies", stop)
 
 func _process(delta) -> void:
 	time += delta
-	msec = fmod(time, 1) * 100
-	seconds = fmod(time, 60)
-	minutes = fmod(time, 3600) / 60
+	msec = int(fmod(time, 1) * 100)
+	seconds = int(fmod(time, 60))
+	minutes = int(fmod(time, 3600) / 60)
 	$Minutes.text = "%02d:" % minutes
 	$Seconds.text = "%02d." % seconds
 	$Msecs.text = "%03d" % msec
